aac5895fe64444fe4d913d80c114b1720aa8e779
daQiuQiu <ren_yi92@sina.com>
2015-12-23 11:52:07 +0800

    Initial Commit

---
+++ Git-Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git-Test
+//
+//  Created by 易仁 on 15/12/23.
+//  Copyright © 2015年 易仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
